f2395f4c4a314cd105c147bf1b7318e7848f4ccf
Edi <egoetschel@clickwork.ch>
2020-08-02 00:41:05 +0200

    Create plugin.php

---
+++ footnotes/plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+class pluginFootnotes extends Plugin {
+
+	public function siteHead(){
+
+		$html  = '<script src="/bl-plugins/footnotes/js/footnoted.js"></script>';
+		return $html;
+
+	}
+
+}
